23118d0e5e5fb74a32d862fbd396d7ceba364c17
Ojochogwu <justyouandickson@gmail.com>
2023-01-13 23:59:27 +0100

    fix-form

--- pages/PageComponents/Home.js
+++ pages/PageComponents/Home.js
@@ -31,21 +31,42 @@ const Container = ({ children }) => {
           </Heading>
         </Box>
         <Box
+          width={500}
+          border="1px"
+          borderColor="gray.400"
           my={4}
           textAlign="left"
-          bg="teal.100"
+          bg="gray.100"
           paddingTop={50}
           paddingLeft={30}
           paddingRight={30}
           paddingBottom={50}
+          borderRadius={6}
         >
           <form>
             <FormControl>
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" />
-              <FormHelperText>We'll never share your email.</FormHelperText>
+              <FormLabel fontWeight={500} mb="10px">
+                Email address
+              </FormLabel>
+              <Input
+                type="email"
+                variant="unstyled"
+                h={40}
+                borderRadius={4}
+                border="1px"
+                borderColor="gray.500"
+                padding="10px"
+                fontSize="14"
+              />
+              <FormHelperText
+                fontSize={12}
+                letterSpacing="0.3px"
+                fontStyle="italic"
+              >
+                Your Email is Safe
+              </FormHelperText>
             </FormControl>
-            <Button width="full" mt={4} type="submit">
+            <Button width="full" mt={12} type="submit">
               Submit
             </Button>
           </form>
